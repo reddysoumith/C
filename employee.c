d739d596c313aca23b2d58182bce5b40f7756f6c
reddysoumith <40242467+reddysoumith@users.noreply.github.com>
2020-09-22 14:40:04 +0530

    Create employee.c

---
+++ employee.c
@@ -0,0 +1,104 @@
+
+       #include<stdio.h>
+
+
+       struct Employee
+       {
+              int Id;
+              char Name[25];
+              int experience;
+              long Salary;
+       };
+
+       void main()
+       {
+
+              struct Employee E;
+
+                    printf("\nEnter Employee Id : ");
+                    scanf("%d",&E.Id);
+
+                    printf("\nEnter Employee Name : ");
+                    scanf("%s",&E.Name);
+
+                    printf("\nEnter Employee experience : ");
+                    scanf("%d",&E.experience);
+
+                    printf("\nEnter Employee Salary : ");
+                    scanf("%ld",&E.Salary);
+
+                    printf("\n\nEmployee Id : %d",E.Id);
+                    printf("\nEmployee Name : %s",E.Name);
+                    printf("\nEmployee experience : %d",E.experience);
+                    printf("\nEmployee Salary : %ld",E.Salary);
+
+                    printf("\nEnter Employee Id : ");
+                    scanf("%d",&E.Id);
+
+                    printf("\nEnter Employee Name : ");
+                    scanf("%s",&E.Name);
+
+                    printf("\nEnter Employee experience : ");
+                    scanf("%d",&E.experience);
+
+                    printf("\nEnter Employee Salary : ");
+                    scanf("%ld",&E.Salary);
+
+                    printf("\n\nEmployee Id : %d",E.Id);
+                    printf("\nEmployee Name : %s",E.Name);
+                    printf("\nEmployee experience : %d",E.experience);
+                    printf("\nEmployee Salary : %ld",E.Salary);
+
+                    printf("\nEnter Employee Id : ");
+                    scanf("%d",&E.Id);
+
+                    printf("\nEnter Employee Name : ");
+                    scanf("%s",&E.Name);
+
+                    printf("\nEnter Employee experience : ");
+                    scanf("%d",&E.experience);
+
+                    printf("\nEnter Employee Salary : ");
+                    scanf("%ld",&E.Salary);
+
+                    printf("\n\nEmployee Id : %d",E.Id);
+                    printf("\nEmployee Name : %s",E.Name);
+                    printf("\nEmployee experience : %d",E.experience);
+                    printf("\nEmployee Salary : %ld",E.Salary);
+
+                    printf("\nEnter Employee Id : ");
+                    scanf("%d",&E.Id);
+
+                    printf("\nEnter Employee Name : ");
+                    scanf("%s",&E.Name);
+
+                    printf("\nEnter Employee experience : ");
+                    scanf("%d",&E.experience);
+
+                    printf("\nEnter Employee Salary : ");
+                    scanf("%ld",&E.Salary);
+
+                    printf("\n\nEmployee Id : %d",E.Id);
+                    printf("\nEmployee Name : %s",E.Name);
+                    printf("\nEmployee experience : %d",E.experience);
+                    printf("\nEmployee Salary : %ld",E.Salary);
+
+                    printf("\nEnter Employee Id : ");
+                    scanf("%d",&E.Id);
+
+                    printf("\nEnter Employee Name : ");
+                    scanf("%s",&E.Name);
+
+                    printf("\nEnter Employee experience : ");
+                    scanf("%d",&E.experience);
+
+                    printf("\nEnter Employee Salary : ");
+                    scanf("%ld",&E.Salary);
+
+                    printf("\n\nEmployee Id : %d",E.Id);
+                    printf("\nEmployee Name : %s",E.Name);
+                    printf("\nEmployee experience : %d",E.experience);
+                    printf("\nEmployee Salary : %ld",E.Salary);
+
+
+       }
